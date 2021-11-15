998f1719726edbd30d5693186cd916c5e80d8290
Marvin Ingel <ingelmarvin@gmail.com>
2021-11-15 22:34:24 +0100

    added tablebinding to orderproducts in order view

--- webapp/controller/Order.controller.js
+++ webapp/controller/Order.controller.js
@@ -10,6 +10,25 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("onlineshop.adminspace.controller.Order", {
+        onInit: function () {
+            const oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+            oRouter.getRoute("order").attachPatternMatched(this._onObjectMatched, this);
+        },
+        _onObjectMatched: function (oEvent) {
+            this.getView().bindElement({
+                path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").orderPath),
+                model: "orders"
+            });
+
+            this._setModelOnTable();
+        },
+        _setModelOnTable: function () {
+            const oData = {
+                "products": this.getView().getBindingContext("orders").getObject().products
+            };
+            const oModel = new sap.ui.model.json.JSONModel(oData);
+            this.getView().setModel(oModel, "orderproducts");
+        },
         onNavBack: function (oEvent) {
             const oHistory = History.getInstance();
             const sPreviousHash = oHistory.getPreviousHash();
@@ -21,7 +40,6 @@ sap.ui.define([
                 oRouter.navTo("orders", {}, true)
             }
         },
-
         onSave: function (oEvent) {
 
         }
