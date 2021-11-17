de403f221f504519559e2dab067c89a727191047
Marvin Ingel <ingelmarvin@gmail.com>
2021-11-17 18:54:30 +0100

    added update order state logic

--- webapp/controller/Order.controller.js
+++ webapp/controller/Order.controller.js
@@ -43,8 +43,47 @@ sap.ui.define([
                 oRouter.navTo("orders", {}, true)
             }
         },
-        onSave: function (oEvent) {
 
+        onPayed: function (oEvent) {
+            const oData = this._getModelBindingData();
+            oData.payed = true;
+
+            this._updateOrderState(oData);
+        },
+
+        onSent: function (oEvent) {
+            const oData = this._getModelBindingData();
+            oData.sent = true;
+
+            this._updateOrderState(oData);
+        },
+
+        _updateOrderState: function (oData) {
+            $.ajax({
+                url: "http://localhost:3000/orders",
+                type: "PUT",
+                contentType: 'application/json',
+                data: JSON.stringify(oData),
+                success: (sResult) => {
+                    sap.m.MessageToast.show(sResult);
+                    this._refreshOrderModel();
+                },
+                error: (oError) => {
+                    sap.m.MessageToast.show("Fehler beim Speichern der Bestellung!");
+                }
+            });
+        },
+
+        _getModelBindingData: function () {
+            const sPath = this.getView().getElementBinding('orders').sPath;
+            return this.getView().getModel('orders').getProperty(sPath);
+        },
+
+        _refreshOrderModel: function () {
+            $.get("http://localhost:3000/orders", (oResponseData, sStatus) => {
+                this.getView().getModel("orders").setData(oResponseData);
+                this.getView().getModel("orders").refresh(true);
+            });
         }
     });
 });
--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,8 +1,6 @@
 sap.ui.define([], function () {
     "use strict"
     return {
-
-
         payedText: function (bStatus) {
             if (bStatus) {
                 return "Bezahlt";
@@ -10,7 +8,6 @@ sap.ui.define([], function () {
                 return "Offen";
             }
         },
-
         payedState: function (bStatus) {
             if (bStatus) {
                 return "Success";
@@ -18,7 +15,6 @@ sap.ui.define([], function () {
                 return "Warning";
             }
         },
-
         deliveryText: function (bStatus) {
             if (bStatus) {
                 return "Versendet";
@@ -26,7 +22,6 @@ sap.ui.define([], function () {
                 return "Nicht versendet";
             }
         },
-
         deliveryState: function (bStatus) {
             if (bStatus) {
                 return "Success";
@@ -34,12 +29,20 @@ sap.ui.define([], function () {
                 return "Warning";
             }
         },
-
-        deliveryButton: function (bPayed, bSent) {
+        deliveryButtonEnabled: function (bPayed, bSent) {
             if (bPayed && !bSent) {
                 return true;
             } else {
                 return false;
+            }
+        },
+        deliveryButtonHover: function (bPayed, bSent) {
+            if (bPayed && !bSent) {
+                return "";
+            } else if (bPayed && bSent) {
+                return "Bestellung wurde bereits als versendet markiert";
+            } else {
+                return "Bestellung muss erst als bezahlt markiert werden"
             }
         }
     }
